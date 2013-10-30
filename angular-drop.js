e6166dfaeeaab5b552ecbe7ce1b2852ab689f7ed
Caitlin Potter <snowball@defpixel.com>
2013-10-30 05:57:01 -0400

    gh-pages v0.0.1-snapshot

--- angular-drop.js
+++ angular-drop.js
@@ -1,7 +1,7 @@
 
 
 /**
- * @license AngularDrop v0.0.1-a7a66ed
+ * @license AngularDrop v0.0.1-68e5506
  * (c) 2013 Caitlin Potter & Contributors. http://caitp.github.io/angular-drop
  * License: MIT
  */
@@ -22,7 +22,7 @@
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var _version = {
-  full: '0.0.1-a7a66ed',    // all of these placeholder strings will be replaced by grunt's
+  full: '0.0.1-68e5506',    // all of these placeholder strings will be replaced by grunt's
   major: '0',    // package task
   minor: '0',
   dot: '1',
